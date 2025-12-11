fdfcdd126b188989f28f46b4e6f4c603ab240040
Nazim Fataliev <nazim.rabota@bk.ru>
2025-12-11 18:35:25 +0300

    добавление гугл метрики

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "@/components/Footer/Footer";
 import "./i18n";
 import I18nProvider from "@/components/I18nProvider/I18nProvider";
 import Head from "next/head";
+import Script from "next/script";
 
 
 export const metadata: Metadata = {
@@ -33,7 +34,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+<Script
+  id="gtm-script"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-5XM76WQV');
+    `,
+  }}
+/>
+
+      </head>
       <body>
+  <noscript>
+    <iframe
+      src="https://www.googletagmanager.com/ns.html?id=GTM-5XM76WQV"
+      height="0"
+      width="0"
+      style={{ display: "none", visibility: "hidden" }}
+    ></iframe>
+  </noscript>
               <I18nProvider>
           <NavHeader/>
         <main>
